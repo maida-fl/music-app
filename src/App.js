811802e159b463faec53829eef9883e55eaaca4b
maida-fl <maida.guzman@outlook.com>
2023-01-26 18:50:56 -0300

    basic components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Song from "./components/Song";
+import Player from "./components/Player";
+
 function App() {
   return (
     <div className="App">
       <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
