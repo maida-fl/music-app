6123ac509587437f7917f1abb3b8a52d48e60bbb
maida-fl <maida.guzman@outlook.com>
2023-01-27 15:45:23 -0300

    basic layout and scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Song from "./components/Song";
 import Player from "./components/Player";
+import "./styles/app.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Music Player</h1>
+    <div className="App">   
       <Song />
       <Player />
     </div>
